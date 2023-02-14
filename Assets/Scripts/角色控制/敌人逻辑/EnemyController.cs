6df180b1cd61fb95537ed81ddc741507ecc26cc2
cxw299 <747502532@qq.com>
2023-02-14 23:41:26 +0800

    战斗系统
    刚做完敌人基础攻击）手感有点怪

--- Assets/Scripts/角色控制/敌人逻辑/EnemyController.cs
+++ Assets/Scripts/角色控制/敌人逻辑/EnemyController.cs
@@ -11,18 +11,22 @@ public class EnemyController : BasicController
     {
         base.Awake();
         characterData = Instantiate(tempData);
+        characterData.敏捷 = 1;
+        SetAnimatorSpeed(0, "Enemy_Attack", 1 / characterData.攻击速度);
+        SetAnimatorSpeed(0, "Enemy_Defence", 1 / characterData.攻击速度);
     }
-    private void OnEnable()
+    private void Start()
     {
         FightManager.Instance.enemyData = characterData;
         FightManager.Instance.enemyContorller = this;
+        FightManager.Instance.enemyAni = ani;
     }
     private void Update()
     {
         switch (enemyStats)
         {
             case EnemyStats.Attack:
-                ReturnToAttackPos(FightManager.Instance.playerController.gameObject.transform.position);
+                ReturnToAttackPos(FightManager.Instance.playerController.startPos);
                 break;
             case EnemyStats.Defence:
                 ReturnToStartPos();
